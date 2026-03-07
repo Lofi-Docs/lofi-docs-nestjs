ef11e490df520617a6dd530e449cecc789b849a0
lofi <jsj2505@gmail.com>
2026-03-07 16:36:53 +0900

    [chore] configuration env

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello! Lofi-Docs API"', () => {
+      expect(appController.getHello()).toBe('Hello! Lofi-Docs API');
     });
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const listenPort = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(listenPort);
+  console.log(`[Log] Server is running on port ${listenPort}`);
 }
-bootstrap();
+void bootstrap();
