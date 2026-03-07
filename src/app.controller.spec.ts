0d8ec93a4a449944f7d4f68de903e0924ffa82d9
lofi <jsj2505@gmail.com>
2026-03-07 16:54:33 +0900

    [test] add health test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +8,7 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -17,6 +19,14 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello! Lofi-Docs API"', () => {
       expect(appController.getHello()).toBe('Hello! Lofi-Docs API');
+    });
+  });
+
+  describe('health', () => {
+    it('should return health message with correct port', () => {
+      expect(appController.getHealth()).toBe(
+        `OK Lofi-Docs Server is running on port ${process.env.PORT ?? 3000}`,
+      );
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello! Lofi-Docs API';
   }
+
+  getHealth(): string {
+    return `OK Lofi-Docs Server is running on port ${process.env.PORT ?? 3000}`;
+  }
 }
