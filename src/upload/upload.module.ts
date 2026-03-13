14cc951154d3800ac0d8a7594831d9cb400fe051
lofi <jsj2505@gmail.com>
2026-03-13 14:27:37 +0900

    [fix] Upload module bug fix

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 @Module({
+  imports: [JwtModule],
   controllers: [UploadController],
   providers: [UploadService],
 })
